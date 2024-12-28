ed2419b50ddcdc20d39e001b1430450bdad9aa56
prwt888 <priyanshrawat888@gmail.com>
2024-12-28 17:26:27 +0530

    minor fixes

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,7 +54,7 @@ export default function Header() {
       <header className="motion-preset-rebound-down motion-delay-[400ms] w-full bg-gradient-to-b from-pink-700 to-pink-500 text-white p-3">
         <div className=" mx-auto  w-full flex flex-wrap justify-between items-center">
           <div className="flex place-items-stretch  md:flex items-center w-full md:w-auto mb-4 md:mb-0">
-            <div className="rounded-full h-[80px] w-[110px] md:h-[120px] md:w-[120px] mr-1 md:mr-5 overflow-auto motion-preset-rebound-right motion-delay-[200ms] shadow-xl">
+            <div className="rounded-full h-[80px] w-[110px] md:h-[120px] md:w-[120px] mr-1 md:mr-5 overflow-auto motion-preset-rebound-right motion-delay-[200ms] shadow-xl  min-w-[80px] min-h-[80px]">
               <Link href="/">
                 <Image
                   src="/logo.png"
@@ -70,19 +70,20 @@ export default function Header() {
                 en="Sitaram Seva Sansthan"
                 hi="सीताराम सेवा संस्थान"
                 tailwindStyles={{
-                  en: "font-serif font-extrabold text-nowrap text-[26px] md:text-5xl mt-4 inline motion-preset-rebound-down motion-delay-[800ms]",
+                  en: "font-serif md:font-serif font-extrabold text-nowrap text-[22px] md:text-5xl mt-1 md:mt-0 inline motion-preset-rebound-down motion-delay-[800ms]",
                   hi: "text-3xl md:text-6xl text-nowrap font-bold"
                 }}
               /></span>
-              <span ><LanguageSwitch
+              <span className='md:motion-preset-rebound-right md:motion-delay-[850ms]' ><LanguageSwitch
                 en="Seva se Samadhan..."
                 hi="सेवा से समाधान..."
                 tailwindStyles={{
-                  en: "font-serif text-lg md:text-xl font-medium ml-0 md:ml-2 italic motion-preset-rebound-right motion-delay-[850ms]",
-                  hi: "text-sm md:text-2xl font-medium ml-0 italic inline"
+                  en: "font-serif text-lg md:text-xl font-medium mt-1 md:mt-0 ml-0 md:ml-2 italic -motion-translate-y-loop-50/mirror motion-duration-[6s] md:motion-paused",
+                  hi: "text-sm md:text-2xl font-medium ml-0 italic inline -motion-translate-y-loop-25/mirror motion-duration-[6s] md:motion-paused"
                 }}
               /></span>
-              <div className=" text-xs md:text-xl font-bold motion-preset-rebound-down motion-delay-[850ms]">Reg.No:03/27/01/2596/24</div>
+
+              <div className=" text-xs md:text-xl font-bold  motion-preset-rebound-down motion-delay-[850ms]">Reg.No:03/27/01/2596/24</div>
 
             </h1>
           </div>
@@ -139,7 +140,7 @@ export default function Header() {
             <Menu size={24} />
           </button>
             </div>
-<div className='md:hidden flex space-x-7 w-full'> <PulsatingButton > 
+<div className='md:hidden flex space-x-9 w-full'> <PulsatingButton > 
             <Link href="/donate" className="bg-white text-pink-600 text-center px-6 md:px-9 py-3 rounded-full text-2xl md:text-3xl text-nowrap font-serif font-semibold  transition-colors ">
              <LanguageSwitch
                 en="Donate Now!"
@@ -151,10 +152,7 @@ export default function Header() {
               />
             </Link></PulsatingButton>
           <LanguageToggle  />
-
-          <button className="md:hidden pr-[7px]" onClick={() => setIsSidebarOpen(true)}>
-            <Menu size={24} />
-          </button></div>
+        </div>
         </div>
       </div>
     </header >
