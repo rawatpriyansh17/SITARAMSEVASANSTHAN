366b6bc4acb64495a319abadc710708c4c15243d
prwt888 <priyanshrawat888@gmail.com>
2025-01-10 12:18:29 +0530

    added animations and gradient

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ export default function DonatePage() {
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-pink-100 via-purple-200 to-pink-600">
       <Header />
       <main className="flex-grow mx-auto mt-8 px-4">
-        <p className="motion-scale-in-[0.5] motion-translate-x-in-[26%] motion-translate-y-in-[17%] motion-opacity-in-[0%] motion-rotate-in-[24deg] motion-blur-in-[5px] motion-duration-[0.35s] motion-duration-[0.53s]/scale motion-duration-[0.53s]/translate motion-duration-[0.63s]/rotate motion-delay-1500 text-center  text-transparent bg-clip-text bg-gradient-to-br from-pink-600 to-purple-800">
+        <p className="motion-scale-in-[0.5] motion-translate-x-in-[26%] motion-translate-y-in-[17%] motion-opacity-in-[0%] motion-rotate-in-[24deg] motion-blur-in-[5px] motion-duration-[0.35s] motion-duration-[0.53s]/scale motion-duration-[0.53s]/translate motion-duration-[0.63s]/rotate motion-delay-800 text-center  text-transparent bg-clip-text bg-gradient-to-br from-pink-600 to-purple-800">
           <LanguageSwitch
             en="Sitaram Seva Sansthan is dedicated to supporting women battling cancer, guided by the ideology of 'Seva se Samadhan', which translates to 'Solution through Service.' Our NGO has successfully organized numerous programs in the past, aiming to provide care, assistance, and resources to empower these women in their fight against cancer and improve their quality of life."
             hi=" सीताराम सेवा संस्थान कैंसर से जूझ रही महिलाओं की सहायता के लिए समर्पित है,जिसका मार्गदर्शन 'सेवा से समाधान' की विचारधारा से होता है,जिसका अर्थ है 'सेवा के माध्यम से समाधान।'हमारे एनजीओ ने अतीत में कई कार्यक्रमों का सफलतापूर्वक आयोजन किया है,जिसका उद्देश्य इन महिलाओं को कैंसर के खिलाफ लड़ाई में सशक्त बनाने और उनके जीवन की गुणवत्ता में सुधार करने के लिए देखभाल, सहायता और संसाधन प्रदान करना है। "
@@ -17,8 +17,8 @@ export default function DonatePage() {
             }}
           />
         </p>
-        <div className="w-full h-[400px] md:h-[725px] relative shadow-2xl rounded-2xl overflow-hidden">
-  <Image src="/groupphoto.png" alt="image not available" layout="fill" objectFit="cover" />
+        <div className=" h-[178px] md:h-[725px] relative shadow-2xl rounded-2xl overflow-hidden motion-preset-rebound-right motion-delay-[840ms]">
+  <Image src="/groupphoto.png" alt="image not available" layout="fill" objectFit="cover" className="sm:object-contain object-contain" />
 </div>
       </main>
       <Footer />
--- app/latest-event/page.tsx
+++ app/latest-event/page.tsx
@@ -1,19 +1,29 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Image from 'next/image'
+import { LanguageSwitch, LanguageToggle } from '@/components/LanguageSwitch'
 
 export default function LatestEventPage() {
   return (
     <div className="min-h-screen bg-pink-50">
       <Header />
-      <h1 className="text-5xl font-bold underline text-pink-800 mt-3 text-center">आगामी कार्यक्रम:</h1>
+      <h1 className="text-5xl font-bold underline text-pink-800 mt-3 text-center">
+        <LanguageSwitch
+                                          en="Upcoming Programs:"
+                                          hi="आगामी कार्यक्रम:"
+                                          tailwindStyles={{
+                                            en: " font-mono md:text-5xl text-2xl font-extrabold  text-pink-800 mt-3 text-center",
+                                            hi: " md:text-5xl font-mono text-3xl font-bold text-pink-800 mt-3 text-center"
+                                          }}
+                                        />
+      </h1>
       <div className="flex items-center justify-center p-1">
         <div className="bg-white rounded-lg shadow-md p-2 w-full sm:w-auto mt-4 sm:mt-8"> {/* Adjusted margin */}
-          <div style={{ width: '100%', maxWidth: '600px' }}>
+          <div >
             <Image
-              src="/flyer.jpeg"
+              src="/flyer.png"
               alt="आगामी कार्यक्रम का फ्लायर"
-              width={600}
+              width={1200}
               height={800}
               layout="responsive"
               className="rounded"
